db22b8f658ea2028a4b58d2432dc49c9bb2a71b7
Rishat <81845@mail.ru>
2021-03-26 17:52:28 +0300

    add gem simplecov add user test scope

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :test do
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   gem 'webdrivers'
+  gem 'simplecov', require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '#validations' do
+  describe 'validations' do
     context 'require field' do
       it 'ensures username presence' do
         user = build(:user, username: '')
-        user.valid?
+        expect(user.save).to eq(false)
         expect(user.errors.full_messages).to include("Username can't be blank")
       end
 
       it 'ensures first_name presence' do
         user = build(:user, first_name: '')
-        user.valid?
+        expect(user.save).to eq(false)
         expect(user.errors.full_messages).to include("First name can't be blank")
       end
 
       it 'ensures last_name presence' do
         user = build(:user, last_name: '')
-        user.valid?
+        expect(user.save).to eq(false)
         expect(user.errors.full_messages).to include("Last name can't be blank")
+      end
+    end
+  end
+  describe 'methods' do
+    context 'scope' do
+      before(:each) do
+        create_list(:user, 2)
+        create(:user, lock: true)
+      end
+
+      it 'active user' do
+        all_user = User.all
+        active_user = User.all_active
+
+        expect(all_user.size).to eq(3)
+        expect(active_user.size).to eq(2)
+      end
+
+      it 'all user exept one' do
+        user = User.first
+        all_user_except_one = User.all_except(user)
+
+        expect(User.all.size).to eq(3)
+        expect(User.all.include?(user)).to be true
+        expect(all_user_except_one.size).to eq(2)
+        expect(all_user_except_one.include?(user)).to be false
       end
     end
   end
