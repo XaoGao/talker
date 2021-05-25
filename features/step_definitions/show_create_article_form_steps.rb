d886d5058ef33fc78fd91d8ec0d61387b7a93819
Rishat <81845@mail.ru>
2021-05-25 18:49:11 +0300

    Add tests

---
+++ features/step_definitions/show_create_article_form_steps.rb
@@ -0,0 +1,11 @@
+Given('I am logged in') do
+  new_session
+end
+
+When('I click button new article') do
+  page.find('#article-form-field').click
+end
+
+Then('I should see a form for new article') do
+  expect(page).to have_content('Добавить новость')
+end
--- features/support/user_helper.rb
+++ features/support/user_helper.rb
@@ -1,14 +1,7 @@
-Given('I am on signin page and user exist') do
+def new_session
   FactoryBot.create(:user, email: 'test@test.com', password: 'password')
   visit '/users/sign_in'
-end
-
-When('I write email\/password and click submit') do
   fill_in 'user_email',	with: 'test@test.com'
   fill_in 'user_password',	with: 'password'
   click_on 'user_signin_submit'
-end
-
-Then('I should see welcome message') do
-  expect(page).to have_content('Signed in successfully.')
 end
