57d85aa1ac40b7d7776b648a4070f6a668a8fe91
Rishat <81845@mail.ru>
2021-08-12 16:40:03 +0300

    add users helper spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -39,19 +39,20 @@
 #
 FactoryBot.define do
   factory :user do
-    first_name    { Faker::Name.first_name }
-    last_name     { Faker::Name.last_name }
-    username      { Faker::Internet.username }
-    password      { 'password' }
-    status        { Faker::Books::Dune.quote }
-    interests     { Faker::Lorem.paragraph(sentence_count: 3) }
-    date_of_birth { Faker::Date.between(from: '1992-09-23', to: '1998-09-25') }
-    lock          { false }
-    email         { Faker::Internet.email }
-    gender        { rand(4) }
-    country       { Faker::Address.community }
-    city          { Faker::Address.city }
-    online        { false }
-    role          { User.roles[:user] }
+    first_name      { Faker::Name.first_name }
+    last_name       { Faker::Name.last_name }
+    username        { Faker::Internet.username }
+    password        { 'password' }
+    status          { Faker::Books::Dune.quote }
+    interests       { Faker::Lorem.paragraph(sentence_count: 3) }
+    date_of_birth   { Faker::Date.between(from: '1992-09-23', to: '1998-09-25') }
+    lock            { false }
+    email           { Faker::Internet.email }
+    gender          { rand(4) }
+    country         { Faker::Address.community }
+    city            { Faker::Address.city }
+    online          { false }
+    role            { User.roles[:user] }
+    last_sign_in_at { DateTime.now }
   end
 end
---
+++ spec/helpers/users_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe UsersHelper, type: :helper do
+  context '.online_label' do
+    let(:user_offline_without_last_sign_in) { build(:user, online: false, last_sign_in_at: nil) }
+    let(:user_offline_with_last_sign_in) { build(:user, online: false, last_sign_in_at: DateTime.new(2001, 2, 3, 4, 5, 6, '+7')) }
+    let(:user_online) { build(:user, online: true, last_sign_in_at: DateTime.new(2001, 2, 3, 4, 5, 6, '+7')) }
+
+    it 'when user online' do
+      expect(online_label(user_online)).to eq('online')
+    end
+
+    it 'when user offline and have not last_sign_in_at' do
+      expect(online_label(user_offline_without_last_sign_in)).to eq('offline')
+    end
+
+    it 'when user offline and have last_sign_in_at' do
+      expect(online_label(user_offline_with_last_sign_in)).to eq(time_ago_in_words(user_offline_with_last_sign_in.last_sign_in_at))
+    end
+  end
+end
