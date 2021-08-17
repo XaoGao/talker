619db737633d604da664468742572ce656eac8e5
Rishat <81845@mail.ru>
2021-08-17 22:31:26 +0300

    Add rspec to controller users to action get index

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    context 'with sign in user' do
+      before(:each) do
+        sign_in create(:user)
+      end
+
+      it 'should be success response' do
+        get users_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'without sign in user' do
+      it 'should be redirect to root path' do
+        get users_path
+        expect(response).to have_http_status(302)
+      end
+    end
   end
 end
--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,6 +1,8 @@
 require_relative './controller_macros'
+require_relative './devise_request_spec_helpers'
 
 RSpec.configure do |config|
+  config.include DeviseRequestSpecHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
 end
