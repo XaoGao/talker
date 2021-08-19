e2d1a4c6223dd106379afa4af5cbaec3f20a5467
Rishat <81845@mail.ru>
2021-08-19 10:14:06 +0300

    add users controller spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,14 +9,65 @@ RSpec.describe 'Users', type: :request do
 
       it 'should be success response' do
         get users_path
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(:ok)
       end
     end
 
     context 'without sign in user' do
       it 'should be redirect to root path' do
         get users_path
-        expect(response).to have_http_status(302)
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
+
+  describe 'GET /show' do
+    let(:user) { create(:user) }
+    context 'with sign in user' do
+      before(:each) do
+        sign_in user
+      end
+      it 'should be success response' do
+        get user_path(user)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'without sign user' do
+      it 'should be redirect to root path' do
+        get user_path(user)
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
+
+  describe 'POST /change_status' do
+    let(:user) { create(:user) }
+
+    context 'without sign user' do
+      it 'should be redirect to root path' do
+        post change_status_user_path(user), params: { status: 'test' }
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
+
+  describe 'GET /photos' do
+    let(:user) { create(:user) }
+    context 'with sign in user' do
+      before(:each) do
+        sign_in user
+      end
+      it 'should be success response' do
+        get photos_user_path(user)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'without sign user' do
+      it 'should be redirect to root path' do
+        get photos_user_path(user)
+        expect(response).to have_http_status(:found)
       end
     end
   end
