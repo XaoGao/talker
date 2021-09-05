5688cfd19363b4a2a406b53ee3fae9ac56d3379d
Rishat <81845@mail.ru>
2021-09-05 16:27:43 +0300

    Add token_authenticate_user

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -2,6 +2,39 @@ class Api::BaseController < ApplicationController
   protect_from_forgery with: :null_session
 
   def token_authenticate_user!
-    true
+    render json: { error: 'Пользователь должен авторизоваться' }, status: :unauthorized unless logged_in?
+  end
+
+  private
+
+  def authorization_header
+    request.headers['Authorization']
+  end
+
+  def token
+    authorization_header.split(' ').compact.last
+  end
+
+  def decoded
+    JwtService::Encoder.decode(token)[0]
+  rescue StandardError => e
+    nil
+  end
+
+  def user_from_token
+    return unless decoded
+
+    return if token_expired?(decoded['exp'])
+
+    user_id = decoded['id']
+    @current_user = User.find(user_id)
+  end
+
+  def logged_in?
+    !!user_from_token
+  end
+
+  def token_expired?(exp)
+    DateTime.now.to_i > exp
   end
 end
