28074a1a7e82db8392ba1343273722b6bfde0318
Rishat <81845@mail.ru>
2021-09-12 11:40:14 +0300

    fix rspec, fix locales for articles

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Api::V1::Articles', type: :request do
     let!(:articles) { create_list(:article, 4, body: 'test') }
     let!(:user) { create(:user) }
 
-    it 'should be a success response', :focus do
+    it 'should be a success response' do
       get api_v1_articles_path, params: { query: 'test' }, headers: RequestHelper.authenticated_header(user)
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body).include?('result')).to be true
