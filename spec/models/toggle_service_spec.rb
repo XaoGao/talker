d1f6a0480421d567ff648a7f5057c27276e4186f
Rishat <81845@mail.ru>
2021-09-19 10:59:41 +0300

    Add toggl_service model rspec

--- spec/models/toggle_service_spec.rb
+++ spec/models/toggle_service_spec.rb
@@ -12,5 +12,23 @@
 require 'rails_helper'
 
 RSpec.describe ToggleService, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe '.service_active?' do
+    context 'when service is exist' do
+      let!(:toggle_service) { create(:toggle_service, name: 'test service', work: true, env: 'test') }
+      it 'should be service is worked' do
+        expect(described_class.service_active?('test service', 'test')).to be true
+      end
+
+      it 'should be service is not work' do
+        toggle_service.update!(work: false)
+        expect(described_class.service_active?('test service', 'test')).to be false
+      end
+    end
+
+    context 'when service is not exist' do
+      it 'should be service is worked' do
+        expect(described_class.service_active?('test service', 'test')).to be false
+      end
+    end
+  end
 end
