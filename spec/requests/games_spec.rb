7f28b0cba53ca98e0f326befe9095724a4062a23
Rishat <81845@mail.ru>
2022-01-04 15:19:46 +0300

    Add auth to games controller and games spec

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "Games", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Games', type: :request do
+  describe 'GET /index' do
+    context 'with sign in user' do
+      let(:user) { create(:user) }
+      before(:each) do
+        sign_in user
+      end
+
+      it 'should be a success response' do
+        get games_path
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'without sign in user' do
+      it 'should be redirect to root path' do
+        get games_path
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
   end
 end
