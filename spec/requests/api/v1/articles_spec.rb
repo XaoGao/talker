8af7ba5050362f19f195821111eed25cb7ef4cf9
Rishat <81845@mail.ru>
2022-01-06 13:24:02 +0300

    fix rswag

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -2,20 +2,16 @@ require 'swagger_helper'
 
 RSpec.describe 'api/v1/articles', type: :request do
   path '/api/v1/articles' do
-    get('list articles') do
+    get 'list articles' do
       tags 'Get articles'
       description 'Find list of articles'
-      consumes 'application/json'
-      produces 'application/json'
-      parameter name: :Authorization,
-                in: :header,
-                type: :string,
-                required: true,
-                description: 'Client token'
-      parameter name: :query, in: :path
-      # response(200, 'successful') do
-      #   run_test!
-      # end
+      security [Bearer: {}]
+      parameter name: :Authorization, in: :header, type: :string, required: true, description: 'Client token'
+      parameter name: :query, in: :path, type: :string, required: true, description: 'Query params'
+
+      response(200, 'successful') do
+        run_test!
+      end
     end
   end
 end
--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -15,9 +15,10 @@ RSpec.describe 'api/v1/sessions', type: :request do
         },
         required: %w[email password]
       }
-      # response(200, 'successful') do
-      #   run_test!
-      # end
+
+      response(200, 'successful') do
+        run_test!
+      end
     end
   end
 end
